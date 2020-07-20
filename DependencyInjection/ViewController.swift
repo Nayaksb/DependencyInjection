96de1c0c6dfa23212bfb855d8f13e37a34cc6e85
SBNAYAK <satya226@gmail.com>
2020-07-20 15:04:20 +0530

    Update ViewController.swift
    Classes Added

--- DependencyInjection/ViewController.swift
+++ DependencyInjection/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let country = CallCountry()
+        country.getCountryDetails()
     }
 
 
 }
+//Mark: API end point
 
+let endPoint = "https://restcountries.eu/rest/v2/name/India"
+    
+
+//Mark: Endpoint Client Defination Class
+
+class EndpointClient{
+    func getCountryData(url: URL, completionHandler:@escaping (_ data: Data?) -> Void){
+        URLSession.shared.dataTask(with: url){ (dataResponse, urlResponse, error) in
+            completionHandler(dataResponse)
+            
+        }.resume()
+    }
+}
+
+class CallCountry{
+    func getCountryDetails(){
+        let clinet: EndpointClient = EndpointClient()
+        
+        clinet.getCountryData(url: URL(string: endPoint)!){(responseData) in
+            if responseData?.count != 0 {
+                //process the response data
+                print("Country Data \(String(describing: responseData?.count))")
+            }
+            
+        }
+    }
+}
